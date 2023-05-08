c10782e809ea50039d15ff028530efae68ee68f2
Denys <hellidengame@gmail.com>
2023-05-08 12:57:44 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css"
 import {ResetPassword} from "./auth-service/components/ResetPassword";
 import {Activation} from "./auth-service/components/Activation";
 import Navigation from "./main-page-service/components/navigation";
+import Header from "./main-page-service/components/header";
 
 
 
@@ -23,6 +24,7 @@ function App() {
             <Route path="/resetpassword" element={<ResetPassword/>}/>
             <Route path="/activation" element={<Activation/>}/>
             <Route path="/navigation" element={<Navigation/>}/>
+            <Route path="/header" element={<Header/>}/>
           </Routes>
         </BrowserRouter>
 
---
+++ src/main-page-service/components/header.js
@@ -0,0 +1,37 @@
+import "../stylesheets/header.css"
+import { ReactComponent as Logo } from '../images/farm-machine-tractor-vehicle-svgrepo-com.svg';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-container">
+                <div className="main-name">
+                    <Logo/>
+                    <h1>Farmobile</h1>
+                </div>
+                <div className='main-phrase'>
+                    <h3>
+                        <i>
+                            Історія зустрічає майбутнє: наші технології - ваш успіх!
+                        </i>
+                    </h3>
+                </div>
+                <div className='call-service'>
+                    <div className='first-telef-number'>
+                        <h2>(055) 555-55-55</h2>
+                        <h4>Відділ продажу запчастин</h4>
+                    </div>
+                    <div className='second-telef-number'>
+                        <h2>(033) 333-33-33</h2>
+                        <h4>Відділ продажу техніки</h4>
+                    </div>
+                    <div className='leaveRequest-button-container'>
+                        <button className='leaveRequest-button' type='submit'>Залишити заявку</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
