f54015369b57f2dd0adb246a048723e142a28664
vlad shevtsov <vshevtsov2022@gmail.com>
2023-05-09 11:28:54 +0300

    some fixes

--- src/main-page-service/components/header.js
+++ src/main-page-service/components/header.js
@@ -1,6 +1,10 @@
 import "../stylesheets/header.css"
 import { ReactComponent as Logo } from '../images/farm-machine-tractor-vehicle-svgrepo-com.svg';
 import {Link} from "react-router-dom";
+import { Link as ScrollLink } from 'react-scroll';
+
+
+
 
 const Header = () => {
     return (
@@ -27,8 +31,18 @@ const Header = () => {
                         <h4>Відділ продажу техніки</h4>
                     </div>
                 </div>
+
                 <div className='leaveRequest-button-container'>
-                    <button className='leaveRequest-button' style={{marginRight: 25 }} type='submit'>Залишити заявку</button>
+
+                         <ScrollLink
+                             to='question-container'
+                             spy={true}
+                             smooth={true}
+                             offset={-70}
+                             duration={1500}
+                             className='leaveRequest-button'
+                             style={{marginRight: 25  }}
+                         >Залишити заявку</ScrollLink>
                     <Link to='/'>
                     <button className='leaveRequest-button' type='submit'>Увійти</button></Link>
                 </div>
