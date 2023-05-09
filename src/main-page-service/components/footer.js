2dfe62c12ab152a85d2eaaf4a2b404c4b925fa8a
Denys <hellidengame@gmail.com>
2023-05-09 19:32:11 +0300

    changed footer

--- src/main-page-service/components/footer.js
+++ src/main-page-service/components/footer.js
@@ -1,5 +1,5 @@
 import "../stylesheets/footer.css"
-import companyLogo from "../images/farm-machine-tractor-vehicle-svgrepo-com.svg"
+import { ReactComponent as CompanyLogo } from '../images/farm-machine-tractor-vehicle-svgrepo-com.svg';
 
 const Question = () => {
     return (
@@ -7,9 +7,16 @@ const Question = () => {
         <footer>
             <div className='blur'>
                 <div className='ourCompany-container'>
+                        <CompanyLogo/>
                     <div className='companyName'>
-                        <img src={companyLogo} className='companyLogo'/>
-                        <label>Farmobile</label>
+                        <label className='mainName'>Farmobile</label>
+                        <label className='tagline'>Історія зустрічає майбутнє: наші технології - ваш успіх!</label>
+                    </div>
+                    <div className='additional-info'>
+                        <i>< a href="mailto:farmobile@gmail.com">farmobile@gmail.com< /a></i>
+                        <span className='telefon'> <br/>(067) 473-58-66</span>
+                        <span className='workTime'><br/>Пн-Пт з 9.00 до 18.00
+                        <br/>Сб-Нд вихідний</span>
                     </div>
                 </div>
             </div>
